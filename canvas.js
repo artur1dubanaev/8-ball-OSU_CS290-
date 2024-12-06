e5a0b9e9e90236dd8df1372504a21273859bd7f5
artur1dubanaev <dubanaea@oregonstate.edu>
2024-12-05 16:54:10 -0800

    1-4

---
+++ canvas.js
@@ -0,0 +1,25 @@
+function Canvas2D(){
+    this._canvas = document.getElementById('screen');
+    this._canvasContext = this._canvas.getContext('2D');
+}
+
+Canvas2D.prototype.clear = function(){
+    this._canvasContext.clearRect(0,0, this._canvas.width, this._canvas.height);
+}
+
+Canvas2D.prototype.drawImage = function(){
+    this._canvasContext.drawImage(image, position.x, position.y);
+}
+
+let Canvas = new Canvas2D();
+
+//Testing
+let image = new Image();
+image.src = "./assets/sprites/spr_background.png";
+
+Canvas.drawImage(image, {x:0,y:0});
+
+
+setTimeout(() => {
+    Canvas.drawImage(image, {x:0,y:0});
+}, 1000);
