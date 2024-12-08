be72c839a77b1b257e7b89bd84045d9f397328a6
artur1dubanaev <dubanaea@oregonstate.edu>
2024-12-07 18:45:01 -0800

    Update Mouse.js

--- input/Mouse.js
+++ input/Mouse.js
@@ -5,7 +5,7 @@ function handleMouseMove(evt){
     let x = evt.pageX;
     let y = evt.pageY;
 
-    Mouse.position = new Vector2(x,y);
+    Mouse.position = new Vector2(x ,y);
 }
 
 function handleMouseDown(evt){
